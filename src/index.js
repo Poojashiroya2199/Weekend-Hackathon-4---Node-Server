6fbe614cfd746eca43fda43c4f0f4b9d2d51ea42
pooja shiroya <67506850+Poojashiroya2199@users.noreply.github.com>
2020-11-07 18:27:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,22 @@
-// var http = require("http");
-
-// const httpServer = http.createServer(handleServer);
-
-
-// function handleServer(req, res) {
-  
-// }
-
-// module.exports = httpServer;
 var http = require("http");
-
 const httpServer = http.createServer(handleServer);
-
-
 function handleServer(req, res) {
   if(req.url=='/welcome'){
   res.write('Welcome to Dominose');
-  res.end();
+  //res.end();
   }
   else if(req.url=='/contact'){
     res.write(JSON.stringify({
         phone: '18602100000',
         email: 'guestcaredominos@jublfood.com'
         }));
-    res.end();
+  ///  res.end();
   }
   else{
 res.write("404");
-res.end();
+//res.end();
   }
-
 }
-
-console.log('listening for client request....');
+//console.log('listening for client request....');
 httpServer.listen(8081);
 module.exports = httpServer;
